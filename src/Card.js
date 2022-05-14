b16861df5861b1c3cf7a7718e8a7d50da03e5cf5
Martin Dablander <martin_dablander@Martins-iMac.local>
2022-05-14 18:12:38 +0200

    Added CardList Component

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ const Card = ({name, email, id}) => {
     // const { name, email, id} = props;
     return (
         <div className="bg-light-green dib br3 ma2 pa3 grow bw2 shadow-5 tc">
-            <img alt="{props.name}" src={`https://robohash.org/${id}test?size=200x200`} />
+            <img alt="{name}" src={`https://robohash.org/${id}test?size=200x200`} />
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
