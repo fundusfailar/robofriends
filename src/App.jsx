a6056df02b9fd11251aae28efb1ce6dd41f48412
Martin Dablander <martin_dablander@Martins-iMac.local>
2022-05-19 18:05:38 +0200

    fetch robot users from API and made CardList scrollable

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
-import { robots } from './robots';  
 import SearchBox from './SearchBox';
 import './app.css';
+import Scroll from './Scroll';
 
 
 class App extends  Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
-    
+   
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }));
+    }
+
     onSearchCange = (event)  => {
         this.setState({ searchfield: event.target.value}) //Methode von React
     }
@@ -22,13 +28,20 @@ class App extends  Component {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        return (
-            <div className="tc">
-                <h1 className="f2">Robo Friends</h1>
-                <SearchBox searchChange={this.onSearchCange}/>
-                <CardList robots={ filteredRobots } />
-            </div>
-        );
+        if (this.state.robots.length === 0) {
+            return <h1>Loading Users</h1>
+        } else {
+            return (
+                <div className="tc">
+                    <h1 className="f2">Robo Friends</h1>
+                    <SearchBox searchChange={this.onSearchCange}/>
+                    <Scroll>
+                        <CardList robots={ filteredRobots } />
+                    </Scroll>
+                </div>
+            );
+        }
+        
     } 
        
 }
