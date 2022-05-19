66b2319a2bae796afcc3bf9f7a0565d1c2c39f69
Martin Dablander <martin_dablander@Martins-iMac.local>
2022-05-19 18:22:45 +0200

    changed folder structure

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import CardList from "./CardList";
-import SearchBox from './SearchBox';
-import './app.css';
-import Scroll from './Scroll';
+import CardList from "../components/CardList";
+import SearchBox from '../components/SearchBox';
+import './App.css';
+import Scroll from '../components/Scroll';
 
 
 class App extends  Component {
